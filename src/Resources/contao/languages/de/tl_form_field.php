31301ff48420414e2348e8f0838573acfbf245c9
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-11-02 11:52:00 +0100

    updated translations

--- src/Resources/contao/languages/de/tl_form_field.php
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -6,6 +6,9 @@
  * @license LGPL-3.0-or-later
  */
 
-$lang = &$GLOBALS['TL_LANG']['FFL'];
+$lang = &$GLOBALS['TL_LANG']['tl_form_field'];
 
-$lang['multifileupload'] = ['Datei-Upload (Multi File Upload)', 'Ein Eingabefeld zur Übertragung mehrerer lokaler Dateien mit Drag & Drop Unterstützung auf den Server.'];
+$lang['mf_maxFiles'][0] = 'Maximale Anzahl an Dateien';
+$lang['mf_maxFiles'][1] = 'Maximale Anzahl an Dateien, welche hochgeladen werden dürfen.';
+$lang['mf_maxFileSize'][0] = 'Maximale Dateigröße in MB';
+$lang['mf_maxFileSize'][1] = 'Die maximale Dateigröße in Megabyte. Das Limit gilt pro Datei.';
--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$fflLang = &$GLOBALS['TL_LANG']['FFL'];
+$fflLang['multifileupload'][0] = 'File-Upload (Multi File Upload)';
+$fflLang['multifileupload'][1] = 'An input field for transferring several local files with drag & drop support to the server.';
+
 $GLOBALS['TL_LANG']['MSC']['dropzone']['labels'] = [
     'head' => '',
     'body' => 'To upload a file, drop it here or click the field.',
@@ -8,27 +18,27 @@ $GLOBALS['TL_LANG']['MSC']['dropzone']['labels'] = [
 
 // dots in order to prevent contao from doing a replaceInsertTags() and remove them
 $GLOBALS['TL_LANG']['MSC']['dropzone']['messages'] = [
-    'dictDefaultMessage'           => 'Drop files here to upload',
-    'dictFallbackMessage'          => 'Your browser does not support drag\'n\'drop file uploads.',
-    'dictFallbackText'             => 'Please use the fallback form below to upload your files like in the olden days.',
-    'dictFileTooBig'               => 'File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.',
-    'dictInvalidFileType'          => 'Wrong file type.',
-    'dictResponseError'            => 'Server responded with {{statusCode}} code.',
-    'dictCancelUpload'             => 'Cancel upload',
+    'dictDefaultMessage' => 'Drop files here to upload',
+    'dictFallbackMessage' => 'Your browser does not support drag\'n\'drop file uploads.',
+    'dictFallbackText' => 'Please use the fallback form below to upload your files like in the olden days.',
+    'dictFileTooBig' => 'File is too big ({{filesize}}MiB). Max filesize: {{maxFilesize}}MiB.',
+    'dictInvalidFileType' => 'Wrong file type.',
+    'dictResponseError' => 'Server responded with {{statusCode}} code.',
+    'dictCancelUpload' => 'Cancel upload',
     'dictCancelUploadConfirmation' => 'Are you sure you want to cancel this upload?',
-    'dictRemoveFile'               => 'Remove file',
-    'dictRemoveFileConfirmation'   => null,
-    'dictMaxFilesExceeded'         => 'You can not upload any more files.',
+    'dictRemoveFile' => 'Remove file',
+    'dictRemoveFileConfirmation' => null,
+    'dictMaxFilesExceeded' => 'You can not upload any more files.',
 ];
 
-$GLOBALS['TL_LANG']['ERR']['illegalMimeType']              = 'Illegal mime type: %s';
-$GLOBALS['TL_LANG']['ERR']['illegalFileExtension']         = 'Illegal file extension: %s';
-$GLOBALS['TL_LANG']['ERR']['invalidUuid']                  = 'No unique identifier (uuid) has been assigned to the file, please try to upload the file again.';
-$GLOBALS['TL_LANG']['ERR']['moveUploadFile']               = 'Invalid file or error on moving files, check permissions or upload destination.';
-$GLOBALS['TL_LANG']['ERR']['outsideUploadDirectory']       = 'Upload destination lies outside the contao upload directory.';
+$GLOBALS['TL_LANG']['ERR']['illegalMimeType'] = 'Illegal mime type: %s';
+$GLOBALS['TL_LANG']['ERR']['illegalFileExtension'] = 'Illegal file extension: %s';
+$GLOBALS['TL_LANG']['ERR']['invalidUuid'] = 'No unique identifier (uuid) has been assigned to the file, please try to upload the file again.';
+$GLOBALS['TL_LANG']['ERR']['moveUploadFile'] = 'Invalid file or error on moving files, check permissions or upload destination.';
+$GLOBALS['TL_LANG']['ERR']['outsideUploadDirectory'] = 'Upload destination lies outside the contao upload directory.';
 $GLOBALS['TL_LANG']['ERR']['uploadNoUploadFolderDeclared'] = 'No valid "uploadFolder" in eval for field "%s" declared, files were not moved from "%s".';
-$GLOBALS['TL_LANG']['ERR']['noUploadFolderDeclared']       = 'No "uploadFolder" in eval for field "%s" declared.';
-$GLOBALS['TL_LANG']['ERR']['minWidth']                     = 'The width of the image must not be less than %s pixel (current image width: %s pixel).';
-$GLOBALS['TL_LANG']['ERR']['minHeight']                    = 'The height of the image must not be less than %s pixel (current image width: %s pixel).';
-$GLOBALS['TL_LANG']['ERR']['maxWidth']                     = 'The width of the image must not exceed %s pixel (current image width: %s pixel).';
-$GLOBALS['TL_LANG']['ERR']['maxHeight']                    = 'The height of the image must not exceed %s pixel (current image width: %s pixel).';
+$GLOBALS['TL_LANG']['ERR']['noUploadFolderDeclared'] = 'No "uploadFolder" in eval for field "%s" declared.';
+$GLOBALS['TL_LANG']['ERR']['minWidth'] = 'The width of the image must not be less than %s pixel (current image width: %s pixel).';
+$GLOBALS['TL_LANG']['ERR']['minHeight'] = 'The height of the image must not be less than %s pixel (current image width: %s pixel).';
+$GLOBALS['TL_LANG']['ERR']['maxWidth'] = 'The width of the image must not exceed %s pixel (current image width: %s pixel).';
+$GLOBALS['TL_LANG']['ERR']['maxHeight'] = 'The height of the image must not exceed %s pixel (current image width: %s pixel).';
